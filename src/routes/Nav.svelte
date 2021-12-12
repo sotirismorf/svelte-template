523b20ad0a6588f545de8772610b52861bf7c05c
Sotiris Morfakidis <sotirismorfakidis@gmail.com>
2021-12-12 21:10:31 +0200

    added 404 page

--- src/routes/Nav.svelte
+++ src/routes/Nav.svelte
@@ -39,22 +39,18 @@
 					aria-expanded="false"
 				>
 					<span class="sr-only">Open main menu</span>
-					<!--
-					Icon when menu is closed.
-					Heroicon name: outline/menu
-					Menu open: "hidden", Menu closed: "block"
-					-->
+					<!-- Icon when menu is closed. Menu open: "hidden", Menu closed: "block" -->
+					{#if !showMobileNavMenu}
 					<svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
 						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
 					</svg>
-					<!--
-					Icon when menu is open.
-					Heroicon name: outline/x
-					Menu open: "block", Menu closed: "hidden"
-					-->
-					<svg class="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+					{/if}
+					<!-- Icon when menu is open. Menu open: "block", Menu closed: "hidden" -->
+					{#if showMobileNavMenu}
+					<svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
 						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
 					</svg>
+					{/if}
 				</button>
 			</div>
 			<div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
@@ -62,37 +58,37 @@
 					<img class="block lg:hidden h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Workflow">
 					<img class="hidden lg:block h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg" alt="Workflow">
 				</div>
-			<div class="hidden sm:block sm:ml-6">
-				<div class="flex space-x-4">
-					<!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-					{#each navigation as item}
-						<a
-							href={item.href}
-							class="{item.current ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white" } px-3 py-2 rounded-md text-sm font-medium"
-						>
-							{item.name}
-						</a>
-					{/each}
+				<div class="hidden sm:block sm:ml-6">
+					<div class="flex space-x-4">
+						<!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+						{#each navigation as item}
+							<a
+								href={item.href}
+								class="{item.current ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white" } px-3 py-2 rounded-md text-sm font-medium"
+							>
+								{item.name}
+							</a>
+						{/each}
+					</div>
 				</div>
 			</div>
-		</div>
-		<!--Notification Button-->
-		<div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-			<button 
-				 type="button" 
-				 class="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-			>
-				<span class="sr-only">View notifications</span>
-				<!-- Heroicon name: outline/bell -->
-				<svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-					<path stroke-linecap="round"
-						  stroke-linejoin="round"
-						  stroke-width="2"
-						  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
-				</svg>
-			</button>
-			<!-- Profile dropdown -->
-			<Dropdown menuItems={dropdownMenuItems} />
+			<!--Notification Button-->
+			<div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+				<button 
+					 type="button" 
+					 class="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+				>
+					<span class="sr-only">View notifications</span>
+					<!-- Heroicon name: outline/bell -->
+					<svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+						<path stroke-linecap="round"
+							  stroke-linejoin="round"
+							  stroke-width="2"
+							  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+					</svg>
+				</button>
+				<!-- Profile dropdown -->
+				<Dropdown menuItems={dropdownMenuItems} />
 			</div>
 		</div>
 	</div>
---
+++ src/routes/__error.svelte
@@ -0,0 +1,14 @@
+<!-- This example requires Tailwind CSS v2.0+ -->
+<div class="relative bg-white overflow-hidden">
+	<div class="max-w-7xl mx-auto">
+		<main class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-20 lg:mt-28">
+			<div class="text-center">
+				<h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+					<span class="block xl:inline">404</span>
+					<span class="block text-indigo-600 xl:inline">page not found</span>
+				</h1>
+				<p class="mt-3 text-base text-gray-500 sm:mt-5">we are sorry, but the page you requested was not found</p>
+			</div>
+		</main>
+	</div>
+</div>
