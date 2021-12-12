27866df34f758d344ef9d4e90d424807843c963c
Sotiris Morfakidis <sotirismorfakidis@gmail.com>
2021-12-12 05:08:13 +0200

    inital commit message

---
+++ src/routes/NavButton.svelte
@@ -0,0 +1,10 @@
+<script>
+	export let href;
+	export let title;
+</script>
+
+<a href={href}
+	class="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-gray-400 items-center justify-center hover:bg-gray-900 hover:text-white"
+>
+<span>{title}</span>
+</a>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import '../tailwind.css'
+	import Nav from './Nav.svelte'
+</script>
+
+<Nav />
+
+<slot></slot>
+
+<style>
+	@tailwind base;
+	@tailwind components;
+</style>
